8028f47d3cc627f0aebeebd160fce26c7e696675
ToastBreadMan <xghostx2709@gmail.com>
2021-09-05 00:21:17 +0200

    added enviroment

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from authentication.models import CustomUser
+
+def login():
+    return HttpResponse("Hello World")
--- django_grade_compare/settings.py
+++ django_grade_compare/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&d&r8!m2ss^s1nzj^5&i9s9f%x@36_pe4%g6#4z@ez=0p+8l8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
